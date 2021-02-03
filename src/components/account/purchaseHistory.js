5bffec19ff3ee0ae159a62a84b15a8cea5d5cfaa
davegordon34 <davegordon34@live.com>
2021-02-02 17:21:45 -0700

    updated the child component for purchase history

--- src/components/account/purchaseHistory.js
+++ src/components/account/purchaseHistory.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 import PageTitle from '../pageTitle';
+import PurchaseDetail from './purchaseDetails';
+import Purchases from './purchases';
+
 
 class PurchaseHistory extends Component {
     render() {
         return (
             <div className='purchase-history'>
                 <PageTitle className='purchase-history__page_title' title='Purchase History'/>
+                <Purchases className='purchase-history__purchases' />
+                <PurchaseDetail classeName='purchase-history__purchaseDetails' />
+                <div className='purchase-history__border-bottom'></div>
             </div>
         )
     }
